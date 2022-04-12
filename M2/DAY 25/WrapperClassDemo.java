f648b79ae96b84c12669703a340071f9e3a003b0
Kiran-512 <kiranyadav267@gmail.com>
2022-04-12 16:53:53 +0530

    Wrapper classes

--- M2/DAY 25/WrapperClassDemo.java
+++ M2/DAY 25/WrapperClassDemo.java
@@ -23,25 +23,41 @@ step2:Integer to Integer
     //above conversion was not possible before 1.5 version
 
     byte b = 10;
-    Integer i2 = b; //CE :incompatible type
+    // Integer i2 = b; //CE :incompatible type
+    Byte b1 = b;
     /*
     byte =>Byte
     Integer = Byte
     */
 
+    System.out.println(b1);
+
     short s = 10;
-    Integer i3 = s; //CE :incompatible type
+    // Integer i3 = s; //CE :incompatible type
+    Short s1 =s;
     /*
     short => Short
     Integer = Short
     */
+    System.out.println(s1);
 
     int x = 10;
-    Byte b1 = x; //CE :incompatible type
+    // Byte b1 = x; //CE :incompatible type
+    Integer x1=x;
     /*
     int => Integer
     Byte = Integer // here wrapper class being final can not inherit any other class so the Integer to Byte conversion is not possible
     */
+    System.out.println(x1);
 
   }
 }
+/*
+[Running] cd "c:\CDAC\Github\180-days-of-code\M2\DAY 25\" && javac WrapperClassDemo.java && java WrapperClassDemo
+10
+10
+10
+10
+10
+
+*/
