bc597f2c64b1ee79f7c92453025d13dc68224b36
Kiran-512 <kiranyadav267@gmail.com>
2022-04-18 22:11:44 +0530

    Data Structure Basics, Array

---
+++ M3/DAY 35/Notes.java
@@ -0,0 +1,49 @@
+Algorithm and Data Structures
+=============================
+Problem Definition => Alogrithm => Data Structure => Program
+=============================
+Algorithm :
+- the finite set of instructions which are being carried in 
+a specific order to perform the specific task.
+
+Characteristics of Algorithm:
+1.Input - There must be at least one input either at compile 
+tiime or at run time
+2.Output- Based on input 1/more O/P will generate
+3.Unambiguity - There must not be unambiguity
+4.Effectiveness - 
+5.Finiteness - 
+
+
+We need algorithms because of the following reasons:
+Scalability
+Performance
+
+
+=============================
+Array :
+- finite,ordered,homogeneous
+- upper bound (end of array) and lower bound(startgin array 
+element postion)
+
+
+Computer Architect give beter clearity on mapping of elements in 
+the memory
+Address(A[i]) = M + (i-L)*8
+Size(A)= U-L+1
+m => Starting location of an Array
+L =>Lower bound
+u => upper bound
+w=> size of the data type
+
+
+SPARSE MATRIX : MATRIX WHICH has only 0 and 1 as an element
+twoD array with the majority of the elements as null
+
+row major order
+column major order
+
+HW:
+Diff between liner and non liner: 
+Short note on ADT:
+=============================
